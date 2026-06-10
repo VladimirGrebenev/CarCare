77341189ab8d21c9d3c65f5ab639d133b277d12e
veneberg81 <veneberg81@sourcecraft.dev>
2026-06-10 09:15:13 +0000

    fix: separate bill_number migration, restore migration_001

--- db/migration_001_add_sts.sql
+++ db/migration_001_add_sts.sql
@@ -1,6 +1,4 @@
--- Миграция: добавляем поля sts и bill_number
+-- Миграция: добавляем поле sts в таблицу cars
 -- Выполнить на существующей БД: psql -U postgres -d carcare -f migration_001_add_sts.sql
 
 ALTER TABLE cars ADD COLUMN IF NOT EXISTS sts VARCHAR(20);
-ALTER TABLE fines ADD COLUMN IF NOT EXISTS bill_number VARCHAR(50) NOT NULL DEFAULT '';
-CREATE INDEX IF NOT EXISTS idx_fines_bill_number ON fines(bill_number);
